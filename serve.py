be01b192ae47b164966f39dd1f35ae37badf34c4
Amir Daniali <amir@amirdaniali.com>
2025-04-09 10:57:38 -0400

    Added python http server

---
+++ serve.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+
+
+import os
+os.chdir("/path/to/the/project/directory")  # Change this to your project folder e.g. E:\Downloads\CTD
+
+PORT = 8080  # Change this port if you want
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
